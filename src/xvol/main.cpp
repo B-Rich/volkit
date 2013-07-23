b4b0649096ccfd92ea993ee338a81552486f9be9
phoboz <alexuspol@hotmail.com>
2013-07-23 22:11:39 +0200

    Cleaned up xvol main program and added check for EXT_texture3D Open GL extension

--- src/xvol/main.cpp
+++ src/xvol/main.cpp
@@ -26,9 +26,7 @@
 #define Y_BRICKS              2
 #define Z_BRICKS              2
 
-#ifdef PLAY
 #define UPDATE_WINDOW_EVENT 99
-#endif
 
 Display *dpy;
 Window win;
@@ -187,293 +185,364 @@ void redraw()
 
 Colormap getSharableColormap(XVisualInfo *vi, Display *dpy)
 {
-  Status status;
-  XStandardColormap *standardCmaps;
-  Colormap cmap;
-  int i, numCmaps;
+    Status status;
+    XStandardColormap *standardCmaps;
+    Colormap cmap;
+    int i, numCmaps;
 
 #ifdef TODO
-  /* True color only */
-  if (vi->class != TrueColor) {
-    fprintf(stderr, "Only true color supported\n");
-    exit(1);
-  }
+    /* True color only */
+    if (vi->class != TrueColor) {
+        fprintf(stderr, "Only true color supported\n");
+        exit(1);
+    }
 #endif
 
-  /* If no standard colormap but TrueColor make an unshared one */
-  status = XmuLookupStandardColormap(dpy, vi->screen, vi->visualid,
-		vi->depth, XA_RGB_DEFAULT_MAP, False, True);
-  if (status == 1) {
-    status = XGetRGBColormaps(dpy, RootWindow(dpy, vi->screen),
-		&standardCmaps, &numCmaps, XA_RGB_DEFAULT_MAP);
-
+    /* If no standard colormap but TrueColor make an unshared one */
+    status = XmuLookupStandardColormap(dpy, vi->screen, vi->visualid,
+                                       vi->depth, XA_RGB_DEFAULT_MAP,
+                                       False, True);
     if (status == 1)
-      for (i = 0; i < numCmaps; i++)
-        if (standardCmaps[i].visualid == vi->visualid) {
-          cmap = standardCmaps[i].colormap;
-          XFree(standardCmaps);
-          return cmap;
+    {
+        status = XGetRGBColormaps(dpy, RootWindow(dpy, vi->screen),
+                                  &standardCmaps, &numCmaps,
+                                  XA_RGB_DEFAULT_MAP);
+        if (status == 1)
+        {
+            for (i = 0; i < numCmaps; i++)
+            {
+                if (standardCmaps[i].visualid == vi->visualid)
+                {
+                    cmap = standardCmaps[i].colormap;
+                    XFree(standardCmaps);
+                    return cmap;
+                }
+            }
         }
-  }
+    }
 
-  cmap = XCreateColormap(dpy, RootWindow(dpy, vi->screen),
-		vi->visual, AllocNone);
-  return cmap;
+    /* Generate new colormap */
+    cmap = XCreateColormap(dpy, RootWindow(dpy, vi->screen),
+                           vi->visual, AllocNone);
+
+    return cmap;
 }
 
-#ifdef PLAY
 void generateTimeoutEvent(Display *dpy, Window win)
 {
-  XEvent event;
-  long event_mask;
+    XEvent event;
+    long event_mask;
 
-  event.type = ClientMessage;
-  event.xclient.type = ClientMessage;
-  event.xclient.serial = 0;
-  event.xclient.send_event = True;
-  event.xclient.display = dpy;
-  event.xclient.window = win;
-  event.xclient.message_type = 0;
-  event.xclient.format = 32;
-  event.xclient.data.l[0] = UPDATE_WINDOW_EVENT;
+    event.type = ClientMessage;
+    event.xclient.type = ClientMessage;
+    event.xclient.serial = 0;
+    event.xclient.send_event = True;
+    event.xclient.display = dpy;
+    event.xclient.window = win;
+    event.xclient.message_type = 0;
+    event.xclient.format = 32;
+    event.xclient.data.l[0] = UPDATE_WINDOW_EVENT;
 
-  event_mask = ExposureMask | StructureNotifyMask;
+    event_mask = ExposureMask | StructureNotifyMask;
 
-  XSendEvent(dpy, win, False, event_mask, &event);
+    XSendEvent(dpy, win, False, event_mask, &event);
 }
-#endif
 
 int main(int argc, char *argv[])
 {
-  GLXContext cx;
-  Bool iconic = False, keepAspect = False;
-  XSizeHints sizeHints = {0};
-  int configuration[] = { GLX_DOUBLEBUFFER, GLX_RGBA,
-  GLX_RED_SIZE, 1, GLX_GREEN_SIZE, 1, GLX_BLUE_SIZE, 1, None};
+    GLXContext cx;
+    Bool iconic = False, keepAspect = False;
+    XSizeHints sizeHints = {0};
+    int configuration[] = {
+        GLX_DOUBLEBUFFER,
+        GLX_RGBA,
+        GLX_RED_SIZE, 1, GLX_GREEN_SIZE, 1, GLX_BLUE_SIZE,
+        1, None
+    };
 
-  XVisualInfo *vi;
-  Colormap cmap;
-  XSetWindowAttributes swa;
-  XWMHints *wmHints;
-  Atom wmDeleteWindow;
-  XEvent event;
-  KeySym ks;
-  char *display = NULL, *geometry = NULL;
-  int flags, x, y, i;
-  int width = DEFAULT_WINDOW_WIDTH;
-  int height = DEFAULT_WINDOW_HEIGHT;
+    XVisualInfo *vi;
+    Colormap cmap;
+    XSetWindowAttributes swa;
+    XWMHints *wmHints;
+    Atom wmDeleteWindow;
+    XEvent event;
+    KeySym ks;
+    char *display = NULL, *geometry = NULL;
+    int flags, x, y, i;
+    int width = DEFAULT_WINDOW_WIDTH;
+    int height = DEFAULT_WINDOW_HEIGHT;
+    char *extensions;
+    int postRedraw = 0;
 
-  /* Process command line */
-  for (i = 0; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      if(!strcmp(argv[i], "-geometry")) {
-        i++;
-        if (i >= argc || argv[i][0] == '-') {
-          fprintf(stderr, "Invalid geometry option\n");
-          exit(1);
+    /* Process command line */
+    for (i = 0; i < argc; i++)
+    {
+        if (argv[i][0] == '-')
+        {
+            if(!strcmp(argv[i], "-geometry"))
+            {
+                i++;
+                if (i >= argc || argv[i][0] == '-')
+                {
+                    fprintf(stderr, "Invalid geometry option\n");
+                    return 1;
+                }
+                geometry = argv[i];
+            }
+            else if(!strcmp(argv[i], "-display"))
+            {
+                i++;
+                if (i >= argc || argv[i][0] == '-')
+                {
+                    fprintf(stderr, "Invalid display option\n");
+                    return 1;
+                }
+                display = argv[i];
+            }
+            else if(!strcmp(argv[i], "-iconic"))
+            {
+                iconic = True;
+            }
+            else if(!strcmp(argv[i], "-keepaspect"))
+            {
+                keepAspect = True;
+            }
+            else if(!strcmp(argv[i], "-single"))
+            {
+                doubleBuffer = False;
+            }
         }
-        geometry = argv[i];
-      }
-      else if(!strcmp(argv[i], "-display")) {
-        i++;
-        if (i >= argc || argv[i][0] == '-') {
-          fprintf(stderr, "Invalid display option\n");
-          exit(1);
+    }
+
+    /* Open connection to XServer */
+    dpy = XOpenDisplay(display);
+    if (dpy == NULL)
+    {
+        perror("Open display");
+        return 1;
+    }
+
+    /* Check for GLX extension */
+    if (!glXQueryExtension(dpy, NULL, NULL))
+    {
+        perror("GLX extension");
+        return 1;
+    }
+
+    /* Find OpenGL capable visual */
+    if (!doubleBuffer)
+    {
+        goto SingleBufferOverride;
+    }
+
+    vi = glXChooseVisual(dpy, DefaultScreen(dpy), configuration);
+    if (vi == NULL)
+    {
+
+/* Label */
+SingleBufferOverride:
+
+        vi = glXChooseVisual(dpy, DefaultScreen(dpy), &configuration[1]);
+        if (vi == NULL)
+        {
+            perror("GLX visual");
+            return 1;
         }
-        display = argv[i];
-      }
-      else if(!strcmp(argv[i], "-iconic"))
-        iconic = True;
-      else if(!strcmp(argv[i], "-keepaspect"))
-        keepAspect = True;
-      else if(!strcmp(argv[i], "-single"))
         doubleBuffer = False;
     }
-  }
+    cmap = getSharableColormap(vi, dpy);
 
-  /* Open connection to XServer */
-  dpy = XOpenDisplay(display);
-  if (dpy == NULL) {
-    perror("Open display");
-    exit(1);
-  }
-
-  /* Check for GLX extension */
-  if (!glXQueryExtension(dpy, NULL, NULL)) {
-    perror("GLX extension");
-    exit(1);
-  }
-
-  /* Find OpenGL capable visual */
-  if (!doubleBuffer)
-    goto SingleBufferOverride;
-  vi = glXChooseVisual(dpy, DefaultScreen(dpy), configuration);
-  if (vi == NULL) {
-  SingleBufferOverride:
-    vi = glXChooseVisual(dpy, DefaultScreen(dpy), &configuration[1]);
-    if (vi == NULL) {
-      perror("GLX visual");
-      exit(1);
-    }
-    doubleBuffer = False;
-  }
-  cmap = getSharableColormap(vi, dpy);
-
-  /* Create OpenGL rendering context */
-  cx = glXCreateContext(dpy, vi, NULL, True);
-  if (cx == NULL) {
-    fprintf(stderr, "Unable to initialize OpenGL rendering context\n");
-    exit(1);
-  }
-
-  /* Setup window */
-  flags = XParseGeometry(geometry, &x, &y,
-	(unsigned int *) &width, (unsigned int *) &height);
-  if (WidthValue & flags) {
-    sizeHints.flags |= USSize;
-    sizeHints.width = width;
-  }
-  if (HeightValue & flags) {
-    sizeHints.flags |= USSize;
-    sizeHints.height = height;
-  }
-  if (XValue & flags) {
-    if (XNegative & flags)
-      x = DisplayWidth(dpy, DefaultScreen(dpy)) + x - sizeHints.width;
-    sizeHints.flags |= USPosition;
-    sizeHints.x = x;
-  }
-  if (YValue & flags) {
-    if (YNegative & flags)
-      y = DisplayHeight(dpy, DefaultScreen(dpy)) + y - sizeHints.height;
-    sizeHints.flags |= USPosition;
-    sizeHints.y = y;
-  }
-  if (keepAspect) {
-    sizeHints.flags |= PAspect;
-    sizeHints.min_aspect.x = sizeHints.max_aspect.x = width;
-    sizeHints.min_aspect.y = sizeHints.max_aspect.y = height;
-  }
-  swa.colormap = cmap;
-  swa.border_pixel = 0;
-  swa.event_mask = ExposureMask | StructureNotifyMask |
-	           KeyPressMask | KeyReleaseMask;
-  win = XCreateWindow(dpy, RootWindow(dpy, vi->screen),
-	sizeHints.x, sizeHints.y, width, height,
-	0, vi->depth, InputOutput, vi->visual,
-	CWBorderPixel | CWColormap | CWEventMask, &swa);
-
-  /* Setup window properties */
-  XSetStandardProperties(dpy, win, "xvol", argv[0],
-	None, argv, argc, &sizeHints);
-  wmHints = XAllocWMHints();
-  wmHints->initial_state = iconic ? IconicState : NormalState;
-  wmHints->flags = StateHint;
-  XSetWMHints(dpy, win, wmHints);
-  wmDeleteWindow = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
-  XSetWMProtocols(dpy, win, &wmDeleteWindow, 1);
-
-  /* Set GLX context */
-  glXMakeCurrent(dpy, win, cx);
-  window_width = width;
-  window_height = height;
-
-  /* Load image */
-  if (load_image(argv[1]) != 0)
-  {
-      fprintf(stderr, "Error - Unable to load image %s\n", argv[1]);
-      return 1;
-  }
-
-  if (argc > 2)
-  {
-    if (colorMap.loadColormap(argv[2]) != 0)
+    /* Create OpenGL rendering context */
+    cx = glXCreateContext(dpy, vi, NULL, True);
+    if (cx == NULL)
     {
-      fprintf(stderr, "Error - Unable to load colormap %s\n", argv[2]);
-      return 1;
+        fprintf(stderr, "Unable to initialize OpenGL rendering context\n");
+        return 1;
     }
-  }
 
-  startup();
+    /* Setup window */
+    flags = XParseGeometry(geometry, &x, &y,
+	                   (unsigned int *) &width,
+                           (unsigned int *) &height);
+    if (WidthValue & flags)
+    {
+        sizeHints.flags |= USSize;
+        sizeHints.width = width;
+    }
 
-  /* Draw window */
-  XMapWindow(dpy, win);
+    if (HeightValue & flags)
+    {
+        sizeHints.flags |= USSize;
+        sizeHints.height = height;
+    }
 
-  /* Event loop */
-  for (;;) {
-    do {
-      XNextEvent(dpy, &event);
-      switch(event.type) {
-        case ConfigureNotify:
-          window_width = event.xconfigure.width;
-          window_height = event.xconfigure.height;
-          init();
-        case Expose:
-	  redraw();
-          break;
-        case KeyPress:
-	  ks = XLookupKeysym((XKeyEvent *) &event, 0);
-          if (ks == XK_Tab || ks == XK_BackSpace)
-          {
-              if (ks == XK_Tab)
-              {
-                  if (++curr_frame > (img->getFrameNr() - 1))
-                  {
-                      curr_frame = img->getFrameNr() - 1;
-                  }
-              }
-              else if (ks == XK_BackSpace)
-              {
-                  if (--curr_frame < 0)
-                  {
-                      curr_frame = 0;
-                  }
-              }
-              printf("Frame: %d\n", curr_frame);
-              img->read(curr_frame);
-              initialized = false;
-              init();
-              redraw();
-          }
-          if (ks == XK_Left)
-          {
-              y_angle += 0.1;
-	      redraw();
-          }
-          else if (ks == XK_Right)
-          {
-              y_angle -= 0.1;
-	      redraw();
-          }
-          if (ks == XK_Down)
-          {
-              x_angle += 0.1;
-	      redraw();
-          }
-          else if (ks == XK_Up)
-          {
-              x_angle -= 0.1;
-	      redraw();
-          }
-          break;
-        case ClientMessage:
-	  if (event.xclient.data.l[0] == wmDeleteWindow) {
-            vr_delete_volume(vd);
-            vr_delete_state(state);
-            exit(0);
-          }
-#ifdef PLAY
-	  else if(event.xclient.data.l[0] == UPDATE_WINDOW_EVENT) {
+    if (XValue & flags)
+    {
+        if (XNegative & flags)
+        {
+            x = DisplayWidth(dpy, DefaultScreen(dpy)) + x - sizeHints.width;
+        }
+        sizeHints.flags |= USPosition;
+        sizeHints.x = x;
+    }
+
+    if (YValue & flags)
+    {
+        if (YNegative & flags)
+        {
+            y = DisplayHeight(dpy, DefaultScreen(dpy)) + y - sizeHints.height;
+        }
+        sizeHints.flags |= USPosition;
+        sizeHints.y = y;
+    }
+
+    if (keepAspect)
+    {
+        sizeHints.flags |= PAspect;
+        sizeHints.min_aspect.x = sizeHints.max_aspect.x = width;
+        sizeHints.min_aspect.y = sizeHints.max_aspect.y = height;
+    }
+
+    swa.colormap = cmap;
+    swa.border_pixel = 0;
+    swa.event_mask = ExposureMask | StructureNotifyMask |
+	             KeyPressMask | KeyReleaseMask;
+
+    win = XCreateWindow(dpy, RootWindow(dpy, vi->screen),
+                        sizeHints.x, sizeHints.y, width, height,
+                        0, vi->depth, InputOutput, vi->visual,
+                        CWBorderPixel | CWColormap | CWEventMask, &swa);
+
+    /* Setup window properties */
+    XSetStandardProperties(dpy, win, "xvol", argv[0],
+                           None, argv, argc, &sizeHints);
+    wmHints = XAllocWMHints();
+    wmHints->initial_state = iconic ? IconicState : NormalState;
+    wmHints->flags = StateHint;
+    XSetWMHints(dpy, win, wmHints);
+    wmDeleteWindow = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(dpy, win, &wmDeleteWindow, 1);
+
+    /* Set GLX context */
+    glXMakeCurrent(dpy, win, cx);
+    window_width = width;
+    window_height = height;
+
+    extensions = (char*) glGetString(GL_EXTENSIONS);
+    if (strstr(extensions, "EXT_texture3D") == NULL)
+    {
+        fprintf(stderr, "No support for 3D textures found\n");
+        return 1;
+    }
+
+    /* Load image */
+    if (load_image(argv[1]) != 0)
+    {
+        fprintf(stderr, "Error - Unable to load image %s\n", argv[1]);
+        return 1;
+    }
+
+    if (argc > 2)
+    {
+        if (colorMap.loadColormap(argv[2]) != 0)
+        {
+            fprintf(stderr, "Error - Unable to load colormap %s\n", argv[2]);
+            return 1;
+        }
+    }
+
+    startup();
+
+    /* Draw window */
+    XMapWindow(dpy, win);
+
+    /* Event loop */
+    for (;;) {
+        do
+        {
+            XNextEvent(dpy, &event);
+            switch(event.type) {
+                case ConfigureNotify:
+                    window_width = event.xconfigure.width;
+                    window_height = event.xconfigure.height;
+                    init();
+                    /* Fall trough */
+
+                case Expose:
+                    postRedraw = 1;
+                    break;
+
+                case KeyPress:
+                    ks = XLookupKeysym((XKeyEvent *) &event, 0);
+                    if (ks == XK_Tab || ks == XK_BackSpace)
+                    {
+                        if (ks == XK_Tab)
+                        {
+                            if (++curr_frame > (img->getFrameNr() - 1))
+                            {
+                                curr_frame = img->getFrameNr() - 1;
+                            }
+                        }
+                        else if (ks == XK_BackSpace)
+                        {
+                            if (--curr_frame < 0)
+                            {
+                                curr_frame = 0;
+                            }
+                        }
+                        printf("Frame: %d\n", curr_frame);
+                        img->read(curr_frame);
+                        initialized = false;
+                        init();
+                        postRedraw = 1;
+                    }
+
+                    if (ks == XK_Left)
+                    {
+                        y_angle += 0.1;
+                        postRedraw = 1;
+                    }
+                    else if (ks == XK_Right)
+                    {
+                        y_angle -= 0.1;
+                        postRedraw = 1;
+                    }
+
+                    if (ks == XK_Down)
+                    {
+                        x_angle += 0.1;
+                        postRedraw = 1;
+                    }
+                    else if (ks == XK_Up)
+                    {
+                        x_angle -= 0.1;
+                        postRedraw = 1;
+                    }
+                    break;
+
+                case ClientMessage:
+                    if (event.xclient.data.l[0] == wmDeleteWindow)
+                    {
+                        vr_delete_volume(vd);
+                        vr_delete_state(state);
+                        exit(0);
+                    }
+                    else if(event.xclient.data.l[0] == UPDATE_WINDOW_EVENT)
+                    {
+                        postRedraw = 1;
+                    }
+                    break;
+            }
+
+        } while (XPending(dpy));
+
+        if (postRedraw)
+        {
             redraw();
-          }
-#endif
-          break;
-      }
-    } while (XPending(dpy));
+            postRedraw = 0;
+        }
+    }
 
-    redraw();
-  }
-
-  return 0;
+    return 0;
 }
 
